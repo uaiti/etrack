6e7836b695e510ab5c0d9392a46444e7cda19789
Wemerson Silva <wemerson.silva@uaiti.com.br>
2015-08-18 18:05:44 -0300

    Implementação do etrack sem jquery

--- public/javascripts/etrack.js
+++ public/javascripts/etrack.js
@@ -1,51 +1,76 @@
+( function( global, undefined ) {
 
-function eTrack(srv_addr, $) {
-	this.session = null;
-	this.customer = null;
-	this.player = null;
-	this.actions = {};
+	global.$ = global.$ || function (query) {
 
-	var that = this;
-	var address = (srv_addr ? srv_addr : 'http://localhost:3030/track/');
-
-	function connect(addr) {
-		var script = document.createElement('script');
-		script.setAttribute('type', 'text/javascript');
-		script.setAttribute('src', addr);
-		script.onload = function() {
-			console.log('registered');
+		global.$.param = function(obj, prefix) {
+			var str = [];
+			for(var p in obj) {
+				var k = prefix ? prefix + "[" + p + "]" : p, v = obj[p];
+				if(v){
+					str.push(typeof v == "object" ?global.$.param(v, k) : encodeURIComponent(k) + "=" + encodeURIComponent(v));
+				}
+			}
+			return str.join("&");
 		}
-		document.body.appendChild(script);
-	}
 
-	// saves the action user executed
-	// {action} is a string that makes sense to your system
-	this.save = function(action, context) {
-		var addr = address + action + '/' + this.session;
-
-		var params = {customer: this.customer, player: this.player, context: context};
-
-		addr += "?" + $.param(params);
-
-		connect(addr);
+		return document.querySelectorAll(query);
 	};
 
-	// tracks the links with data-action="" automatically
-	this.trackLinks = function(attr) {
-		if (!attr)
-			attr = 'action';
-		$(document).on('click', 'a[data-' + attr + ']', function() {
-			that.save($(this).attr('data-' + attr));
-		});
+	function eTrack(srv_addr, $) {
+		this.session = null;
+		this.customer = null;
+		this.player = null;
+		this.actions = {};
+
+		var that = this;
+		var address = (srv_addr ? srv_addr : 'http://localhost:3030/track/');
+
+		function connect(addr) {
+			var script = document.createElement('script');
+			script.setAttribute('type', 'text/javascript');
+			script.setAttribute('src', addr);
+			script.onload = function() {
+				console.log('registered');
+			}
+			document.body.appendChild(script);
+		}
+
+		// saves the action user executed
+		// {action} is a string that makes sense to your system
+		this.save = function(action, context) {
+			var addr = address + action + '/' + this.session;
+
+			var params = {customer: that.customer, player: that.player, context: context};
+			console.log(params)
+			addr += "?" + global.$.param(params);
+
+			connect(addr);
+		};
+
+		// tracks the links with data-action="" automatically
+		this.trackLinks = function(attr) {
+			if (!attr)
+				attr = 'action';
+
+			[].slice.call(global.$('a[data-' + attr + ']')).forEach(function(el,i){
+				el.addEventListener('click', function(){
+					that.save(el.getAttribute('data-' + attr));
+				})
+			});
+		};
+
+		this.setSession = function(sessionId) {
+			that.session = sessionId;
+		};
+		this.setCustomer = function(customer) {
+			that.customer = customer;
+		};
+		this.setPlayer = function(player) {
+			that.player = player;
+		};
 	};
 
-	this.setSession = function(sessionId) {
-		that.session = sessionId;
-	};
-	this.setCustomer = function(customer) {
-		that.customer = customer;
-	};
-	this.setPlayer = function(player) {
-		that.player = player;
-	};
-};
+	// expose access to the constructor
+	window.eTrack = eTrack;
+
+} )( window );
