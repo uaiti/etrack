354f077bbe8aed64b09009e68420c8d841c6d0f5
Charles Schaefer <charlesschaefer@gmail.com>
2015-08-18 12:57:14 -0300

    updating to express 4

--- app.js
+++ app.js
@@ -3,39 +3,75 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , track = require('./routes/track');
+var express = require('express'),
+    routes = require('./routes'),
+    track = require('./routes/track'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    path = require('path'),
+    debug = require('debug')('app4'),
+    logger = require('morgan');
 
-var app = module.exports = express.createServer();
 
-// Configuration
+var app = module.exports = express();
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-//  app.use(express.cookieParser());
-//  app.use(express.session({ secret: 'your secret here' }));
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
+// view engine setup
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
+
+/**
+  app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  });
+
+  app.configure('production', function(){
+    app.use(express.errorHandler());
+  });
+*/
 
 // Routes
-
 app.get('/', routes.index);
 app.get('/track/:type/:session', track.track);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-app.listen(3030, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+var server = app.listen(3030, function(){
+  debug("Express server listening on port %d in %s mode", server.address().port, app.get('env'));
 });
